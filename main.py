7596b3408b96a15f66663dccfe1b93f747fb3606
jdarman <43575668+jdarman@users.noreply.github.com>
2018-09-27 15:07:46 -0400

    Added part of the side wall to the code

--- main.py
+++ main.py
@@ -18,41 +18,54 @@ def main(): #main function
     pygame.display.set_icon(logo)
     #sets the logo as the designated logo
 
-    class Background(pygame.sprite.Sprite):
-        def __init(self):
-            pygame.sprite.Sprite.__init__(self)
-            self.images = []
-            img = pygame.image.load(os.path.join("_assets", "background.png")).convert()
-            self.images.append(img)
-            self.image = self.images[0]
-            self.rect = self.image.get_rect()
-
-
     class Ball(pygame.sprite.Sprite):
     #spawns the ball into the game
         def __init__(self):
             pygame.sprite.Sprite.__init__(self)
             self.images = []
-            img = pygame.image.load(os.path.join("_assets", "ball.png")).convert()
-            self.images.append(img)
+            ballimg = pygame.image.load(os.path.join("_assets", "ball.png")).convert()
+            self.images.append(ballimg)
             self.image = self.images[0]
             self.rect = self.image.get_rect()
+
+    class Paddle(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.images = []
+            paddleimg = pygame.image.load(os.path.join("_assets", "paddle.png")).convert()
+            self.images.append(paddleimg)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+
+    class SideWalls(pygame.sprite.Sprite):
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.images = []
+            sidewallimg = pygame.image.load(os.path.join("_assets", "sideWallTile.png")).convert()
+            self.images.append(sidewallimg)
+            self.images = self.images[0]
+            self.rect = self.image.get_rect()
+
+    background = pygame.image.load(os.path.join("_assets", "background.png")).convert()
+    screen.blit(background, (0,0))        
 
     ball = Ball()
     ball.rect.x = 0
     ball.rect.y = 0
     ball_list = pygame.sprite.Group()
     ball_list.add(ball)
+    #screen.blit(ball, (ball.rect.x, ball.rect.y))
+    ball_list.draw(screen)
 
-    #background = Background()
-    #background.rect.x = 0
-    #background.rect.y = 0
-    #background_list = pygame.sprite.Group()
-    #background_list.add(background)
+    paddle = Paddle()
+    paddle.rect.x = 294
+    paddle.rect.y = 420
+    paddle_list = pygame.sprite.Group()
+    paddle_list.add(paddle)
+    #screen.blit(paddle, (paddle.rect.x, paddle.rect.y))
+    paddle_list.draw(screen)
 
-    #backdrop.blit(backdrop, backdropbox)
-    #ball_list.draw(screen)
-    #pygame.display.flip()
+    pygame.display.flip()
 
     running = True
     while running:
