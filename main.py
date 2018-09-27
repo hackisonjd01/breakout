7295fd4fe00a474074d10182542d02d9bbd39752
jdarman <43575668+jdarman@users.noreply.github.com>
2018-09-27 10:58:31 -0400

    attempted to fix assets, added ball and background objects. also, hello mr. donohue! :)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 #imports pygame so it can be used
 import os
+import sys
 #import os controls so they can be used
 
 def main(): #main function
@@ -8,12 +9,50 @@ def main(): #main function
     #initializes all pygame modules
 
     pygame.display.set_caption("breakout")
-    screen = pygame.display.set_mode((640, 480))
+    w = 640
+    h = 480
+    screen = pygame.display.set_mode((w, h))
     #sets up the window. default resolution is 480p
 
     logo = pygame.image.load("_assets/breakout.jpg")
     pygame.display.set_icon(logo)
     #sets the logo as the designated logo
+
+    class Background(pygame.sprite.Sprite):
+        def __init(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.images = []
+            img = pygame.image.load(os.path.join("_assets", "background.png")).convert()
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+
+
+    class Ball(pygame.sprite.Sprite):
+    #spawns the ball into the game
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.images = []
+            img = pygame.image.load(os.path.join("_assets", "ball.png")).convert()
+            self.images.append(img)
+            self.image = self.images[0]
+            self.rect = self.image.get_rect()
+
+    ball = Ball()
+    ball.rect.x = 0
+    ball.rect.y = 0
+    ball_list = pygame.sprite.Group()
+    ball_list.add(ball)
+
+    #background = Background()
+    #background.rect.x = 0
+    #background.rect.y = 0
+    #background_list = pygame.sprite.Group()
+    #background_list.add(background)
+
+    #backdrop.blit(backdrop, backdropbox)
+    #ball_list.draw(screen)
+    #pygame.display.flip()
 
     running = True
     while running:
@@ -22,8 +61,11 @@ def main(): #main function
                 pygame.quit
                 os._exit(1)
                 running = False
+
+    
     #main run loop
 
 if __name__ == "__main__":
     main()
+
 
